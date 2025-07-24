513a79a9ac3b61aaa03183261dc8188c1c98ab2c
Júlio César Prado <127704877+dvjuliocesar@users.noreply.github.com>
2025-07-24 15:00:10 -0300

    version 3.1
    1) Formatação dos gráficos.
    2) Deleção da Tabela de Proporção de Casos Novos Sigilosos.

--- analise1_processos_judiciais.py
+++ analise1_processos_judiciais.py
@@ -67,106 +67,53 @@ analise_sigilo['Total_Processos'] = analise_sigilo['Nao_Sigilosos'] + analise_si
 analise_sigilo['Proporcao_Sigilosos'] = analise_sigilo['Sigilosos'] / analise_sigilo['Total_Processos'] * 100
 
 # converter ano para inteiro
-analise_sigilo['Ano'] = analise_sigilo['Ano'].astype(int)
-
-# 4) Tabela Resumo
-'''
-print("\nTabela Resumo Anual:")
-for index, row in analise_sigilo.iterrows():
-    print(f"\n{int(row['Ano'])}:") # Convertendo para inteiro antes de formatar
-    print(f"Casos novos: {row['Nao_Sigilosos']:,.0f}".replace(",", "."))  # Separador de milhar como ponto
-    print(f"Casos sigilosos: {row['Sigilosos']:,.0f}".replace(",", "."))
-    print(f"Proporção sigilosos: {row['Proporcao_Sigilosos']:.2f}".replace(".", ",") + "%")
-'''
-
-# Tabela como plotly table
-tabela_resumo = analise_sigilo[['Ano', 'Total_Processos', 'Sigilosos', 'Proporcao_Sigilosos']].copy()
-tabela_resumo.columns = ['Ano', 'Total', 'Sigilosos', '% Sigilosos']
-tabela_resumo['Ano'] = tabela_resumo['Ano'].astype(str)  # Converter ano para string para exibição
-tabela_resumo['Total'] = tabela_resumo['Total'].apply(lambda x: f"{x:,.0f}".replace(",", ".")) # Formatar decimal brasileiro
-tabela_resumo['Sigilosos'] = tabela_resumo['Sigilosos'].apply(lambda x: f"{x:,.0f}".replace(",", "."))
-tabela_resumo['% Sigilosos'] = tabela_resumo['% Sigilosos'].apply(lambda x: f"{x:.2f}".replace(".", ",")) + "%"  # Formatar porcentagem
+analise_sigilo['Ano'] = analise_sigilo['Ano'].astype(str)
 
 
-fig_table = go.Figure(data=[go.Table(
-    header=dict(values=tabela_resumo.columns,  
-                fill_color='paleturquoise',
-                align='left'),  
-    cells=dict(values=[tabela_resumo[col] for col in tabela_resumo.columns],
-               fill_color='lavender',  
-                align='left'))])  
-
-fig_table.update_layout(
-    title_text='<b>Tabela de Resumo Anual dos Processos Sigilosos e Não Sigilosos</b>', 
-    title_x=0.5
-    )    
-
-# Cor do cabeçalho hexadecimal
-fig_table.update_traces(
-    header=dict(
-        fill_color='#203864',  # Cor do cabeçalho em hexadecimal
-        font=dict(color='white', size=16)  # Cor e tamanho da fonte do cabeçalho
-    ),
-    cells=dict(
-        fill_color='lavender',  # Cor das células em hexadecimal
-        font=dict(color='black', size=14)  # Cor e tamanho da fonte das células
-    )
-)
-
-# Configurar o layout da tabela 
-fig_table.update_layout(
-    height=400,  # Altura da tabela
-    width=800,   # Largura da tabela
-    margin=dict(l=20, r=20, t=30, b=20),
-    title_font=dict(size=20, color='black'),  # Tamanho e cor do título
-    font=dict(size=14, color='black')  # Tamanho e cor da fonte
-)
-
-# Exibir tabela
-fig_table.show()  
-
-# 5) Gráficos
-# 5.1) Plotar gráfico em barras de comparação entre processos sigilosos e não sigilosos
-fig = px.bar(
+# 4) Gráficos
+# Plotar gráfico em barras de comparação entre processos sigilosos e não sigilosos
+fig1 = px.bar(
     analise_sigilo,
     x='Ano',
     y=['Nao_Sigilosos', 'Sigilosos'],  
-    title='<b>Comparativo Anual do Número de Processos Sigilosos e Não Sigilosos</b>',
+    title='<b>Comparativo Anual do Número de Casos Novos Sigilosos e Casos Novos Não Sigilosos</b>',
     labels={'value': 'Total de Processos', 'variable': 'Tipo de Processo'},
     color_discrete_sequence=["#4375D3", '#203864'],  # Cores para os tipos de processo
     barmode='group'
     )
 
 # Formatando o Gráfico
-fig.for_each_trace(
-    lambda trace: trace.update(hovertemplate='<b>Total de Casos Novos:</b> %{y:,.0f}<extra></extra>') 
-    if trace.name == 'Nao_Sigilosos' 
-    else trace.update(hovertemplate='<b>Total de Casos Sigilosos:</b> %{y:,.0f}<extra></extra>')
+# Adicionar valores formatados separadamente para cada barra
+fig1.data[0].text = [f"{x:,.0f}".replace(",", ".") for x in analise_sigilo['Nao_Sigilosos']]
+fig1.data[1].text = [f"{x:,.0f}".replace(",", ".") for x in analise_sigilo['Sigilosos']]
+
+fig1.update_traces(
+    hovertemplate=None,  # Remove tooltips
+    hoverinfo='skip',    # Desativa informações ao passar o mouse
+    textfont_size=12,
+    textposition='outside'
 )
 
-fig.update_layout(
+fig1.update_layout(
     separators=',.', # Formatar separador de milhar brasileiro
     title_x=0.5, 
     legend_title_text='Tipo de Processo',
     xaxis_title='Ano',
-    yaxis_title='Total de Processos'
-    ) 
+    yaxis_title='Total de Processos',
+) 
 
-fig.update_xaxes(
-    tickmode='array',
-    tickvals=analise_sigilo['Ano'].unique(),
-    ticktext=analise_sigilo['Ano'].astype(str)
-    ) # Valor do ano, em inteiro, no eixo x
+# Ajustar legenda
+fig1.data[0].name = 'Não Sigilosos'
+fig1.data[1].name = 'Sigilosos'
 
 # 5.2) Gráfico de proporção de processos sigilosos
-# Configurações adicionais
 fig2 = px.bar(
     analise_sigilo,
     x='Ano',
     y='Proporcao_Sigilosos',
     title='<b>Proporção de Processos Sigilosos</b>',
     text='Proporcao_Sigilosos',
-    labels={'Proporcao_Sigilosos': 'Proporção de Sigilosos (%)', 'Ano': 'Ano'},
+    labels={'Proporcao_Sigilosos': 'Proporção de Sigilosos (%)', 'Ano': 'Ano'}
 )
 
 # Definir cores personalizadas para cada barra
@@ -176,12 +123,13 @@ cores_personalizadas = {
     2024: "#203864"
 }
 
-# Aplicar as cores manualmente
+# Formantandp o gráfico
 fig2.update_traces(
-    marker_color=[cores_personalizadas[ano] for ano in analise_sigilo['Ano']],
+    marker_color=[cores_personalizadas[int(ano)] for ano in analise_sigilo['Ano']],
     texttemplate='%{text:.2f}%',
     textposition='outside',
-    hovertemplate='<b>Proporção de Processos Sigilosos:</b> %{y:.2f}%<extra></extra>'
+    hovertemplate=None,  
+    hoverinfo='skip'    
 )
 
 fig2.update_layout(
@@ -198,7 +146,7 @@ fig2.update_xaxes(
     ) # Valor do ano, em inteiro, no eixo x
 
 # Exibir gráficos
-fig.show()
+fig1.show()
 fig2.show()
 
 
