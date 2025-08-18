1c88eb4a8ca7af7f67e28e032c6fe6eeb40d3a0e
Júlio César Prado <127704877+dvjuliocesar@users.noreply.github.com>
2025-08-18 11:40:27 -0300

    Update analise4_processos_judiciais.py

--- analise4_processos_judiciais.py
+++ analise4_processos_judiciais.py
@@ -1,4 +1,4 @@
-'''Análise de Processos Judiciais - Área de Ação e Sigilos:
+'''Análise de Processos Judiciais Sigilosos - Área de Ação agrupadas por Comarcas:
 - Este script analisa dados de processos judiciais, focando no número de processos e
 na proporção de processos sigilosos por Área de Ação. Ele gera tabelas e gráficos para visualização dos dados.'''
 
